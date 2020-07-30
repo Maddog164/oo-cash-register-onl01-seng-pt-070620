6037e83b9e8f217b5b3417bf8bda120853716495
Denise Henderson <denise@henderson3.us>
2020-07-30 03:54:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -46,8 +46,8 @@ class CashRegister
 
     def void_last_transaction
       i = 1
-      while @last_transaction[2]>i do
-        binding.pry
+      while @last_transaction[2]>=i do
+        binding.pry 
         @total = @total-(@last_transaction[1])
         i+=1
       end
